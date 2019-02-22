c004da0fa20cd1252489b77f711db6e2184e802d
Osama Ahmed <osamaahmed694@gmail.com>
2019-02-23 01:51:57 +0200

    adding stories sorting feature

--- js/controllers.js
+++ js/controllers.js
@@ -3,19 +3,20 @@ angular.module('HackerNews.controllers', []).
 /* topStories controller */
 controller('newsController', function($scope, hackerNewsAPIservice) {
   $scope.nameFilter = null;
+  $scope.myOrderBy=null;
+  $scope.IsDesc=false;
   $scope.storiesIds = [];
   $scope.stories= [];
 
  
-  hackerNewsAPIservice.getBestStoriesIds().then(function mySuccess (response) {
+  hackerNewsAPIservice.getBestStoriesIds().then(
+    function mySuccess (response) {
       //Dig into the responde to get the relevant data
-      debugger;
       $scope.storiesIds = response.data;
 
       for(var i=0;i<$scope.storiesIds.length;i++)
       {
         hackerNewsAPIservice.getStory($scope.storiesIds[i]).then(function mySuccess(response){
-          debugger;
           if(response.data.type=="story"){
             response.data.time=$scope.convertUnixTimeToDate(response.data.time);
             $scope.stories.push(response.data);
@@ -23,22 +24,31 @@ controller('newsController', function($scope, hackerNewsAPIservice) {
         });
       }
 
-  }, function myError(response){
-        alert(response);
-  });
-
-  debugger;
-
+    }, 
+    function myError(response){
+      alert(response);
+    }
+  );
   
 
   $scope.searchFilter = function (story) {
     debugger;
     var keyword = new RegExp($scope.nameFilter, 'i');
     return !$scope.nameFilter || keyword.test(story.title) || keyword.test(story.url);
-    };
+  };
 
-    $scope.convertUnixTimeToDate = function(unixTime){
-      var date = new Date(unixTime*1000);
-      return date;
-    }
+  $scope.sortStories = function(orderBy){
+    debugger;
+    if($scope.myOrderBy==orderBy)
+        $scope.IsDesc=!$scope.IsDesc;
+    else
+        $scope.IsDesc=false;
+
+    $scope.myOrderBy=orderBy;
+  }
+
+  $scope.convertUnixTimeToDate = function(unixTime){
+    var date = new Date(unixTime*1000);
+    return date;
+  }
 });
